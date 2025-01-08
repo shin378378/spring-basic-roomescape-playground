0b9afee1bb639d0684937356ef560af2cf2748e1
hyebin <shin378378@naver.com>
2025-01-08 15:44:42 +0900

    feat : 4단계 test 코드 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -5,13 +5,9 @@ import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import roomescape.reservation.dto.ReservationResponse;
-import roomescape.time.Time;
-import roomescape.time.TimeRepository;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -116,5 +112,19 @@ public class MissionStepTest {
                 .get("/admin")
                 .then().log().all()
                 .statusCode(200);
+    }
+
+    @Test
+    void 오단계() {
+        String adminToken = createToken("admin@email.com", "password");
+
+//        List<MyReservationResponse> reservations = RestAssured.given().log().all()
+//                .cookie("token", adminToken)
+//                .get("/reservations-mine")
+//                .then().log().all()
+//                .statusCode(200)
+//                .extract().jsonPath().getList(".", MyReservationResponse.class);
+//
+//        assertThat(reservations).hasSize(3);
     }
 }
