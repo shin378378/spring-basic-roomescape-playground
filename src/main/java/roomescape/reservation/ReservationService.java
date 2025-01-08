cde7be66387a2257f8b4c5689a1c6f3215107762
hyebin <shin378378@naver.com>
2025-01-08 13:44:25 +0900

    feat : reservationRepository 만들기

--- src/main/java/roomescape/reservation/ReservationService.java
+++ src/main/java/roomescape/reservation/ReservationService.java
@@ -3,30 +3,52 @@ package roomescape.reservation;
 import org.springframework.stereotype.Service;
 import roomescape.reservation.dto.ReservationRequest;
 import roomescape.reservation.dto.ReservationResponse;
+import roomescape.theme.Theme;
+import roomescape.time.Time;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ReservationService {
-    private ReservationDao reservationDao;
+    private final ReservationRepository reservationRepository;
 
-    public ReservationService(ReservationDao reservationDao) {
-        this.reservationDao = reservationDao;
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
     }
 
     public ReservationResponse save(ReservationRequest reservationRequest) {
-        Reservation reservation = reservationDao.save(reservationRequest);
+        Reservation reservation = new Reservation(
+                null,
+                reservationRequest.getName(),
+                reservationRequest.getDate(),
+                new Time(reservationRequest.getTime(), null),
+                new Theme(reservationRequest.getTheme(), null, null)
+        );
+        Reservation savedReservation = reservationRepository.save(reservation);
 
-        return new ReservationResponse(reservation.getId(), reservationRequest.getName(), reservation.getTheme().getName(), reservation.getDate(), reservation.getTime().getValue());
+        return new ReservationResponse(
+                savedReservation.getId(),
+                savedReservation.getName(),
+                savedReservation.getTheme().getName(),
+                savedReservation.getDate(),
+                savedReservation.getTime().getValue()
+        );
     }
 
     public void deleteById(Long id) {
-        reservationDao.deleteById(id);
+        reservationRepository.deleteById(id);
     }
 
     public List<ReservationResponse> findAll() {
-        return reservationDao.findAll().stream()
-                .map(it -> new ReservationResponse(it.getId(), it.getName(), it.getTheme().getName(), it.getDate(), it.getTime().getValue()))
-                .toList();
+        return reservationRepository.findAllWithDetails().stream()
+                .map(it -> new ReservationResponse(
+                        it.getId(),
+                        it.getName(),
+                        it.getTheme().getName(),
+                        it.getDate(),
+                        it.getTime().getValue()
+                ))
+                .collect(Collectors.toList());
     }
 }
