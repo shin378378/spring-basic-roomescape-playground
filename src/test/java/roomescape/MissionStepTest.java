0c0a96f85ce451db92b7f259a146292e8e2828ee
hyebin <shin378378@naver.com>
2025-01-09 02:44:50 +0900

    feat : 6단계 테스트 작성

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import roomescape.reservation.dto.MyReservationResponse;
 import roomescape.reservation.dto.ReservationResponse;
+import roomescape.waiting.dto.WaitingResponse;
 
 import java.util.HashMap;
 import java.util.List;
@@ -130,43 +131,43 @@ public class MissionStepTest {
         assertThat(reservations).hasSize(3);
     }
 
-//    @Test
-//    void 육단계() {
-//        String brownToken = createToken("brown@email.com", "password");
-//
-//        Map<String, String> params = new HashMap<>();
-//        params.put("date", "2024-03-01");
-//        params.put("time", "1");
-//        params.put("theme", "1");
-//
-//        // 예약 대기 생성
-//        WaitingResponse waiting = RestAssured.given().log().all()
-//                .body(params)
-//                .cookie("token", brownToken)
-//                .contentType(ContentType.JSON)
-//                .post("/waitings")
-//                .then().log().all()
-//                .statusCode(201)
-//                .extract().as(WaitingResponse.class);
-//
-//        // 내 예약 목록 조회
-//        List<MyReservationResponse> myReservations = RestAssured.given().log().all()
-//                .body(params)
-//                .cookie("token", brownToken)
-//                .contentType(ContentType.JSON)
-//                .get("/reservations-mine")
-//                .then().log().all()
-//                .statusCode(200)
-//                .extract().jsonPath().getList(".", MyReservationResponse.class);
-//
-//        // 예약 대기 상태 확인
-//        String status = myReservations.stream()
-//                .filter(it -> it.getId() == waiting.getId())
-//                .filter(it -> !it.getStatus().equals("예약"))
-//                .findFirst()
-//                .map(it -> it.getStatus())
-//                .orElse(null);
-//
-//        assertThat(status).isEqualTo("1번째 예약대기");
-//    }
+    @Test
+    void 육단계() {
+        String brownToken = createToken("brown@email.com", "password");
+
+        Map<String, String> params = new HashMap<>();
+        params.put("date", "2024-03-01");
+        params.put("time", "1");
+        params.put("theme", "1");
+
+        // 예약 대기 생성
+        WaitingResponse waiting = RestAssured.given().log().all()
+                .body(params)
+                .cookie("token", brownToken)
+                .contentType(ContentType.JSON)
+                .post("/waitings")
+                .then().log().all()
+                .statusCode(201)
+                .extract().as(WaitingResponse.class);
+
+        // 내 예약 목록 조회
+        List<MyReservationResponse> myReservations = RestAssured.given().log().all()
+                .body(params)
+                .cookie("token", brownToken)
+                .contentType(ContentType.JSON)
+                .get("/reservations-mine")
+                .then().log().all()
+                .statusCode(200)
+                .extract().jsonPath().getList(".", MyReservationResponse.class);
+
+        // 예약 대기 상태 확인
+        String status = myReservations.stream()
+                .filter(it -> it.getId() == waiting.getId())
+                .filter(it -> !it.getStatus().equals("예약"))
+                .findFirst()
+                .map(it -> it.getStatus())
+                .orElse(null);
+
+        assertThat(status).isEqualTo("1번째 예약대기");
+    }
 }
