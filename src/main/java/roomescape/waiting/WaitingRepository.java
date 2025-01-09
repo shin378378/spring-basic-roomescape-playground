a87cfc5a01c5a1d6525ec1d4098c96691e65e357
hyebin <shin378378@naver.com>
2025-01-09 13:33:21 +0900

    feat : @Repository어노테이션 붙이기

--- src/main/java/roomescape/waiting/WaitingRepository.java
+++ src/main/java/roomescape/waiting/WaitingRepository.java
@@ -2,9 +2,11 @@ package roomescape.waiting;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface WaitingRepository extends JpaRepository<Waiting, Long> {
 
     @Query("SELECT new roomescape.waiting.WaitingWithRank(" +
